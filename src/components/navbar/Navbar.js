68c79cb3a9d991c57fa899af9c64b3fe0f3b0622
manish <Manishbisht9711@gmail.com>
2022-10-11 12:55:40 +0530

    home page responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../images/logo.svg";
 import Songbar from "../songbar/Songbar";
 
@@ -11,47 +11,64 @@ import music from "../../images/icons/music.svg";
 import playlist from "../../images/icons/playlist.svg";
 import profile from "../../images/icons/profile.svg";
 import radio from "../../images/icons/radio.svg";
+import { BiMenu } from "react-icons/bi";
 
 const Navbar = () => {
+  const [togglenav, settogglenav] = useState(false);
+
   return (
     <nav>
       <Link to="/" aria-label="muscia">
         <img src={logo} alt="musica" />
       </Link>
-      <ul>
-        <li>
-          <Link aria-label="home" to="/">
-            <BiHomeAlt />
-          </Link>
-        </li>
-        <li>
-          <Link aria-label="collection" to="/collection">
-            <img src={playlist} alt="collection-icon" />
-          </Link>
-        </li>
-        <li>
-          <Link aria-label="radio" to="/">
-            <img src={radio} alt="radio-icon" />
-          </Link>
-        </li>
-        <li>
-          <Link aria-label="music" to="/">
-            <img src={music} alt="music-icon" />
-          </Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link aria-label="profile" to="/">
-            <img src={profile} alt="rofile-icon" />
-          </Link>
-        </li>
-        <li>
-          <Link aria-label="logout" to="/">
-            <img src={logout} alt="logout-icon" />
-          </Link>
-        </li>
-      </ul>
+
+      <button
+        className="hamburger"
+        aria-expanded={togglenav}
+        aria-label="toggle-menu"
+        onClick={() => {
+          settogglenav(!togglenav);
+        }}
+      >
+        <BiMenu />
+      </button>
+      <div className="navlinks">
+        <ul>
+          <li>
+            <Link aria-label="home" to="/">
+              <BiHomeAlt />
+            </Link>
+          </li>
+          <li>
+            <Link aria-label="collection" to="/collection">
+              <img src={playlist} alt="collection-icon" />
+            </Link>
+          </li>
+          <li>
+            <Link aria-label="radio" to="/">
+              <img src={radio} alt="radio-icon" />
+            </Link>
+          </li>
+          <li>
+            <Link aria-label="music" to="/">
+              <img src={music} alt="music-icon" />
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link aria-label="profile" to="/">
+              <img src={profile} alt="rofile-icon" />
+            </Link>
+          </li>
+          <li>
+            <Link aria-label="logout" to="/">
+              <img src={logout} alt="logout-icon" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+
       <Songbar />
     </nav>
   );
